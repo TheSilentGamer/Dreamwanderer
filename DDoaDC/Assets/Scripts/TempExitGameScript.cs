140dc14fa6b90da06386d53b926cbfba61bf95bf
Benjamin <ben.bartholo@gmail.com>
2022-08-29 20:10:37 +0930

    Library Speed Edited

---
+++ DDoaDC/Assets/Scripts/TempExitGameScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempExitGameScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
