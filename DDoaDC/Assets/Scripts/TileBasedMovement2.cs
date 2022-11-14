614d0d82b3b40e81e724d188328cb57159453179
Thomas M <thomasdanielmitchell@gmail.com>
2022-11-14 20:24:46 +1030

    updated stuff

--- DDoaDC/Assets/Scripts/TileBasedMovement2.cs
+++ DDoaDC/Assets/Scripts/TileBasedMovement2.cs
@@ -8,6 +8,11 @@ public class TileBasedMovement2 : MonoBehaviour
     public Transform movePoint;
     public LayerMask whatStopsMovement;
 
+   public bool HorizontalIsPressed;
+    public bool HorizontalDown;
+    public bool VerticalIsPressed;
+    public bool VerticalDown;
+
     void Start()
     {
         movePoint.parent = null;
@@ -17,18 +22,48 @@ public class TileBasedMovement2 : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
+        if (Input.GetAxisRaw("Horizontal") != 0f)
+        {
+            if (!HorizontalDown)
+            {
+                HorizontalIsPressed = true;
+                VerticalIsPressed = false;
+                HorizontalDown = true;
+            }
+        }
+        else
+        {
+            HorizontalIsPressed = false;
+            HorizontalDown = false;
+        }
+
+        if (Input.GetAxisRaw("Vertical") != 0f)
+        {
+            if (!VerticalDown)
+            {
+                HorizontalIsPressed = false;
+                VerticalIsPressed = true;
+                VerticalDown = true;
+            }
+        }
+        else
+        {
+            VerticalIsPressed = false;
+            VerticalDown = false;
+        }
+
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && Input.GetAxisRaw("Vertical") == 0)
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .1f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .1f, whatStopsMovement) && HorizontalIsPressed)
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
             }
-             if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && Input.GetAxisRaw("Horizontal") == 0)
+             if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .1f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .1f, whatStopsMovement) && VerticalIsPressed)
                 {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f); 
 
--- DDoaDC/Assets/Scripts/playeranimator.cs
+++ DDoaDC/Assets/Scripts/playeranimator.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class playeranimator : MonoBehaviour
 {
     public Animator animator;
-
+    AudioSource m_playerscrape;
+    public bool m_Play;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m_playerscrape = GetComponent<AudioSource>();
+        m_Play = true;
     }
 
     // Update is called once per frame
@@ -38,8 +40,18 @@ public class playeranimator : MonoBehaviour
         {
             animator.SetBool("IsWalkingUD", false);
         }
-
-
-
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            m_Play = true;
+            m_playerscrape.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            if (!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+            {
+                m_playerscrape.Stop();
+                m_Play = false;
+            }
+        }
     }
 }
